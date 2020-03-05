64fef07e4702ff08a9349405ee715a59a33e9f84
kostya <konstantin.uvar@gmail.com>
2020-03-05 12:57:24 +0300

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication
+from uiControl.MainWindowControl import MainWindowControl
+
+
+def main():
+    app = QApplication([])
+    window = MainWindowControl()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
